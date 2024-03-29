3ccf6a2b1c006442402c44a63dec0bb55a4f6a5f
Asim307 <asimshaikhby0097@gmail.com>
2024-03-29 15:44:48 -0700

    cli based atm program

---
+++ index.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 20000;
+let mypin = 8892;
+let pinans = await inquirer.prompt([{
+        name: "pin",
+        message: "enter 4 digit pin",
+        type: "number",
+    }]);
+if (pinans.pin === mypin) {
+    console.log("correct pin!");
+    let operationans = await inquirer.prompt([{
+            name: "operation",
+            message: "select an option",
+            type: "list",
+            choices: ["withdraw", "check balance", "fast cash"],
+        }]);
+    if (operationans.operation === "withdraw") {
+        let amountans = await inquirer.prompt([{
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            }]);
+        if (amountans.amount > 20000) {
+            console.log("unsufficient balance");
+        }
+        else {
+            mybalance -= amountans.amount;
+            console.log("you have cash withdraw pkr " + amountans.amount + " from your meezan bank account ! your remaining balance is " + mybalance);
+        }
+    }
+    else if (operationans.operation === "check balance") {
+        console.log("your remaining balance is" + mybalance);
+    }
+    if (operationans.operation === "fast cash") {
+        let fastcashans = await inquirer.prompt([{
+                name: "fastcash",
+                message: "please select one option",
+                type: "list",
+                choices: ["5000", "10000", "15000", "20000"],
+            }]);
+        if (fastcashans.fastcash) {
+            console.log("you have cash withdraw pkr " + fastcashans.fastcash);
+        }
+    }
+}
+else {
+    console.log("incorrect pin!");
+}
